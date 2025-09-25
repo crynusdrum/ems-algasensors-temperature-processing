a94736b375aa30d29f0fed0e130d2b413c264107
crynusdrum <crynusdrum@gmail.com>
2025-09-25 15:01:24 +0100

    10.11. Implementando microsserviço de Temperature Processing

--- src/test/java/com/algaworks/algasensors/temperature/processing/UUIDv7Test.java
+++ src/test/java/com/algaworks/algasensors/temperature/processing/UUIDv7Test.java
@@ -1,7 +1,7 @@
 package com.algaworks.algasensors.temperature.processing;
 
-import com.fasterxml.uuid.Generators;
-import com.fasterxml.uuid.impl.TimeBasedEpochRandomGenerator;
+import com.algaworks.algasensors.temperature.processing.common.IdGenerator;
+import com.algaworks.algasensors.temperature.processing.common.UUIDv7Utils;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
